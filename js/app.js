5a8ae0291ee9db465f69872486638c020e8c066a
gene5ive <jedigene@gmail.com>
2015-07-29 11:39:39 -0700

    represent completed code from first tutorial. ready for server and db.

--- js/app.js
+++ js/app.js
@@ -32,3 +32,28 @@ var ContactView = Backbone.View.extend({
     return this;
   }
 });
+
+var DirectoryView = Backbone.View.extend({
+  el: $("#contacts"),
+
+  initialize: function () {
+    this.collection = new Directory(contacts);
+    this.render();
+  },
+
+  render: function () {
+    var that = this;
+    _.each(this.collection.models, function (item) {
+      that.renderContact(item);
+    }, this);
+  },
+
+  renderContact: function (item) {
+    var contactView = new ContactView({
+      model: item
+    });
+    this.$el.append(contactView.render().el);
+  }
+});
+
+var directory = new DirectoryView();
