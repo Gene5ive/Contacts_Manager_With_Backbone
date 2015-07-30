066dd838ed730adda1a31f38fbbd769c2920e0b4
gene5ive <jedigene@gmail.com>
2015-07-29 19:55:02 -0700

    add server config

--- server.js
+++ server.js
@@ -7,16 +7,10 @@ var application_root = __dirname,
 //Create server
 var app = express();
 
-//Configure server
-// app.configure(function() {
-//   app.use(express.bodyParser()); //Parses request body and populates req.body
-//   app.use(express.methodOverride()); //Checks req.body for HTTP method overrides
-//   app.use(app.router); //Perform route lookup basedon url and HTTP method
-//   app.use(express.static(path.join(application_root, "public"))); //Where to serve static content
-//   app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); //Show all error in development
-// });
-
 //Start server
-app.listen(4700, function() {
-  // console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+app.listen(4700);
+
+//Routes
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/public/index.html');
 });
