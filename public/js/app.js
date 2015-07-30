066075b5c1446514739bee1e2a2468dd99fb39b6
gene5ive <jedigene@gmail.com>
2015-07-30 11:00:24 -0700

    have most bugs fixed but some remain.

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,6 @@
 (function ($) {
+
+  //demo data
   var contacts =
      [{ name: "Contact 1", address: "1, a street, a town, a city, AB12 3CD", tel: "0123456789", email: "anemail@me.com", type: "family" },
       { name: "Contact 1", address: "1, a street, a town, a city, AB12 3CD", tel: "0123456789", email: "anemail@me.com", type: "family" },
@@ -7,53 +9,59 @@
       { name: "Contact 1", address: "1, a street, a town, a city, AB12 3CD", tel: "0123456789", email: "anemail@me.com", type: "family" },
       { name: "Contact 1", address: "1, a street, a town, a city, AB12 3CD", tel: "0123456789", email: "anemail@me.com", type: "colleague" },
       { name: "Contact 1", address: "1, a street, a town, a city, AB12 3CD", tel: "0123456789", email: "anemail@me.com", type: "friend" },
-      { name: "Contact 1", address: "1, a street, a town, a city, AB12 3CD", tel: "0123456789", email: "anemail@me.com", type: "family" }],
+      { name: "Contact 1", address: "1, a street, a town, a city, AB12 3CD", tel: "0123456789", email: "anemail@me.com", type: "family" }];
+
+  //define product model
+  var Contact = Backbone.Model.extend({
+    defaults: {
+      photo: "/img/placeholder.png"
+    }
+  });
+
+  //define directory collection
+  var Directory = Backbone.Collection.extend({
+    model: Contact
+  });
+
+  //define individual contact view
+  var ContactView = Backbone.View.extend({
+    tagName: "article",
+    className: "contact-container",
+    template: $("#contactTemplate").html(),
+
+    render: function() {
+      var tmpl = _.template(this.template);
+
+      $(this.el).html(this.template(this.model.toJSON()));
+      return this;
+    }
+  });
+
+  //define master view
+  var DirectoryView = Backbone.View.extend({
+    el: $("#contacts"),
+
+    initialize: function () {
+      this.collection = new Directory(contacts);
+      this.render();
+    },
+
+    render: function () {
+      var that = this;
+      _.each(this.collection.models, function (item) {
+        that.renderContact(item);
+      }, this);
+    },
+
+    renderContact: function (item) {
+      var contactView = new ContactView({
+        model: item
+      });
+      $(this.el).append(contactView.render().el);
+    }
+  });
+
+  //create instance of master view
+  var directory = new DirectoryView();
+
 } (jQuery));
-
-var Contact = Backbone.Model.extend({
-  defaults: {
-    photo: "/img/placeholder.png"
-  }
-});
-
-var Directory = Backbone.Collection.extend({
-  model: Contact
-});
-
-var ContactView = Backbone.View.extend({
-  tagName: "article",
-  className: "contact-container",
-  template: $("#contactTemplate").html(),
-
-  render: function() {
-    var tmpl = _.template(this.template);
-
-    this.$el.html(tmpl(this.model.toJSON()));
-    return this;
-  }
-});
-
-var DirectoryView = Backbone.View.extend({
-  el: $("#contacts"),
-
-  initialize: function () {
-    this.collection = new Directory(contacts);
-    this.render();
-  },
-
-  render: function () {
-    var that = this;
-    _.each(this.collection.models, function (item) {
-      that.renderContact(item);
-    }, this);
-  },
-
-  renderContact: function (item) {
-    var contactView = new ContactView({
-      model: item
-    });
-    this.$el.append(contactView.render().el);
-  }
-});
-
-var directory = new DirectoryView();
